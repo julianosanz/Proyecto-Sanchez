114885af5024c6cef0f6dd75930130ff138fb5ee
julinosanz <julianos_sanz@hotmail.com>
2022-07-14 21:10:40 -0300

    CDA a CLI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,197 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" id="app">
+    <div class="row">
+
+            <div class="col-4 col-md-4 offset-4 text-center">
+                <h1 class="main-title">Cartelera</h1>
+            </div>
+
+        </div>    
+        
+        <div class="col-4 col-md-4 offset-4 text-center">
+            <h2 class="main-title">Infantiles</h2>
+            <desafio 
+                :peliculas="infantiles" 
+                :estilosnuevo="estiloInfantil"
+                >
+            </desafio>
+        </div>
+
+        <div class="col-4 col-md-4 offset-4 text-center">
+            <h2 class="main-title">Todo publico</h2>
+            <desafio :peliculas="todoPublico" :estilosnuevo="estiloTodo"></desafio>
+        </div>
+
+        <div class="col-4 col-md-4 offset-4 text-center">
+            <h2 class="main-title">18+</h2>
+            <desafio :peliculas="maYores" :estilosnuevo="estilo18"></desafio>
+        </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import desafio from './components/desafio.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    desafio
+  },
+  data(){
+    return {
+      canAccess: true,
+      infantiles: [
+                {
+                
+                id: 1,
+                
+                titulo: "Cars",
+                
+                year: "2006",
+                
+                categoria: "Animacion / Comedia",
+
+                duracion: "96 min"
+                
+                },
+                
+                {
+                
+                id: 2,
+                
+                titulo: "Metegol",
+                
+                year: "2013",
+                
+                categoria: "Animacion / Aventura",
+
+                duracion: "106 min"
+                
+                },
+                
+                {
+                
+                id: 3,
+                
+                titulo: "Mi villano favorito",
+                
+                year: "2010",
+                
+                categoria: "Animacion / Comedia",
+
+                duracion: "95 min"
+                
+                }
+    ],
+
+        todoPublico: [
+            {
+            
+            id: 1,
+            
+            titulo: "Spider-Man",
+            
+            year: "2002",
+            
+            categoria: "Accion / Aventura",
+
+            duracion: "120 min"
+            
+            },
+            
+            {
+            
+            id: 2,
+            
+            titulo: "Karate Kid",
+            
+            year: "1984",
+            
+            categoria: "Animacion / Aventura",
+
+            duracion: "122 min"
+            
+            },
+            
+            {
+            
+            id: 3,
+            
+            titulo: "Juego de gemelas",
+            
+            year: "1998",
+            
+            categoria: "Comedia",
+
+            duracion: "110 min"
+            
+            }
+    ],
+
+        maYores: [
+            {
+            
+            id: 1,
+            
+            titulo: "John Wick 3",
+            
+            year: "2019",
+            
+            categoria: "Accion",
+
+            duracion: "131 min"
+            
+            },
+            
+            {
+            
+            id: 2,
+            
+            titulo: "Proyecto X",
+            
+            year: "2012",
+            
+            categoria: "Comedia",
+
+            duracion: "88 min"
+            
+            },
+            
+            {
+            
+            id: 3,
+            
+            titulo: "Ambulancia",
+            
+            year: "2022",
+            
+            categoria: "Suspenso",
+
+            duracion: "136 min"
+            
+            }
+            
+    ],
+
+    estiloInfantil: 
+        {
+            'border': '1px solid',
+            'background' : '#FF5733',
+            'color': 'white'
+        },
+    estiloTodo: 
+        {
+            'border': '2px solid blue',
+            'background' : '#565F07',
+            'color': '#2EFF00'
+        },
+    estilo18: 
+        {
+            'border': '1px solid red'
+            ,'background' : '#00FFCD',
+            'color': '#FF0000'
+        }
+    }
   }
 }
 </script>
@@ -25,4 +205,40 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.footer {
+            position: sticky;
+            bottom:0;
+            width: 100%;
+  }
+  .card {
+
+    min-width: 220px;
+    
+    padding: 20px 10px;
+    
+    margin: 10px 10px;
+    
+    font-size: large;
+    
+    color:grey;
+    
+    border-color:palegreen;
+    
+    border-style: solid;
+    
+    border-width: 1px;
+    
+    max-width: 220px;
+    
+    background-color: whitesmoke !important;
+    
+    transition: 0.7s;
+    
+    }
+    
+    .card:hover {
+    
+    box-shadow: 0 6px 10px 0 rgba(0,0,0,0.2);
+    
+    }
 </style>
---
+++ src/components/desafio.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+
+  <section class="desafio">
+ 
+  <table :style="estilosnuevo">
+            <thead>
+                <tr :style="estilosnuevo">
+                    <th :style="estilosnuevo"> Titulo </th>
+                    <th :style="estilosnuevo"> Año </th>
+                    <th :style="estilosnuevo"> Categoria </th>
+                    <th> Duracion </th>
+                </tr>
+            </thead>
+
+            <tbody>
+            
+                <tr v-for="o in peliculas" :key="o.id"> 
+                    <td> {{ o.titulo }} </td>
+                    <td> {{ o.year }} </td>
+                    <td> {{ o.categoria }} </td>
+                    <td> {{ o.duracion }} </td>
+                </tr>
+            
+            </tbody>
+        </table>
+        </section>
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'desafioComponente',
+    props: {
+        titulo: String,
+        portada: String,
+        puntaje: String,
+        peliculas: Array,
+        estilosnuevo: Object
+        
+    },
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .desafio {
+
+  }
+</style>
